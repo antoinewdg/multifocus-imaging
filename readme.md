337a3007c833536df1ba8e037c53062e5d1925b3
antoinewdg <antoinewendlinger@gmail.com>
2016-09-25 17:26:40 +0200

    Minor tweaks

--- readme.md
+++ readme.md
@@ -4,6 +4,11 @@ Implementation of the paper
 [Multi-focus imaging using local focus estimation and mosaicking](https://vision.ece.ucsb.edu/sites/vision.ece.ucsb.edu/files/publications/fedorov_focus_icip2006.pdf)
 as a school project.
 
+A big part of the project relies on the paper 
+[A Multiresolution Spline With Application to
+ Image Mosaics](http://www.cs.toronto.edu/~mangas/teaching/320/assignments/a3/spline83.pdf).
+
+
 Use of the [OpenCV 3.1](http://opencv.org/opencv-3-1.html) library for image processing routines.
 
 
--- src/splining.h
+++ src/splining.h
@@ -8,15 +8,53 @@
 
 #include "utils.h"
 
-
+/**
+ * Build the generating kernal of parameter a
+ *
+ * Defined in the paper on splining.
+ *
+ * @param a
+ * @return
+ */
 Mat_<float> generating_kernel(float a);
 
+/**
+ * Compute the Gaussian pyramid of an image
+ *
+ * @param src
+ * @param w kernel to be used
+ * @return
+ */
+vector<Mat_<float>> compute_gaussian_pyramid(Mat_<float> src, Mat_<float> w);
+
+/**
+ * Compute the Laplacian pyramid of an image
+ *
+ * @param src
+ * @param w kernel to be used
+ * @return
+ */
 vector<Mat_<float>> compute_laplacian_pyramid(Mat_<float> src, Mat_<float> w);
 
+/**
+ * Merge two grayscale images using multi-resolution splining.
+ *
+ * @param im_a
+ * @param im_b
+ * @return
+ */
 Mat_<float> merge_images(Mat_<float> &im_a, Mat_<float> &im_b);
 
+/**
+ * Merge two colored images using multi-resolution splining.
+ *
+ * Performs the splining on each RGB component.
+ *
+ * @param im_a
+ * @param im_b
+ * @return
+ */
 Mat_<Vec3b> merge_images(Mat_<Vec3b> &im_a, Mat_<Vec3b> &im_b);
 
-vector<Mat_<float>> compute_gaussian_pyramid(Mat_<float> src, Mat_<float> w);
 
 #endif //MULTI_FOCUS_SPLINING_H
